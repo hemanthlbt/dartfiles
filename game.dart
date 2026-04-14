3b015fbc60d5f7610cdb64c82106a19c6557360d
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-04-14 18:09:37 +0530

    kajal

--- game.dart
+++ game.dart
@@ -1,15 +1,22 @@
+// dart data types!!!
+
 void main() {
 
+String name= "hemanth";
+print("String:$name");
 
 
-for(var n=1; n<=10; n++) {
-	print(n);
+String name2 = "Hemanthkumarlingabthina";
+print("String:$name2");
 
-	if (n==5) {
-		print("hemanth");
-	}
+
+
+int num1 = 100;
+int num2 = 200;
+
+print(" addition : $num1 + $num2 ");
+
 }
 
 
-}
 
