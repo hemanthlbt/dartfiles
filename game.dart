9e512af98f199f78fdcd8235270a674f3b99d8dd
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-04-14 17:06:11 +0530

    "This is my first dart file"

---
+++ game.dart
@@ -0,0 +1,31 @@
+void main() {
+	
+	var name1 = "hemanth";
+	var name2 = "kumar";
+	var name3 = "lingabathina";
+
+			print(name1+name2+name3);
+
+
+
+			 final age  = 100;
+				final names  = true;
+
+				var period = false;
+				var period2 = true;
+				var period3 = true;
+
+
+				print(age);
+				print(names);
+				print(period);
+				print(period2);
+				print(period3);
+
+
+
+}
+
+
+
+
