12fdab86458695b47b7fdf02ce959e988f77efaa
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-04-20 01:02:44 +0530

    maps

---
+++ maps.dart
@@ -0,0 +1,24 @@
+void main() {
+
+//maps, key value pairs.
+
+//Show keys.
+
+//Show Length.
+
+//Add something.
+
+
+//add many things.
+
+
+//remove something..
+
+
+//remove everything..
+
+
+
+
+
+}
