04af82d4d72c82b6eeeeac8c3477f99914b0efe4
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-04-20 00:43:21 +0530

    lists

---
+++ lists.dart
@@ -0,0 +1,37 @@
+void main() {
+	//lists
+	var myList = [1,2,3];
+	// print(myList[1]);
+
+
+// change an item.
+myList[0] =100;
+print(myList);
+
+//create an empty list!
+var list2 = [];
+
+//add to empty list!
+list2.add(43);
+
+
+//add multiple to empty list..
+list2.addAll([1,2,3,4,5,6,]);
+
+
+//Insert a specific position..
+list2.insert(0,1000);
+print(list2);
+
+
+//Insertmany.
+
+
+
+//remove from the list.
+
+
+//Remove from specific location!
+
+
+}
